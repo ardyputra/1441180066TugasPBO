51bd9e42defdc08d6bca74c69f91ddca9d132151
ardyputra <ardyputra96@gmail.com>
2015-03-26 16:22:58 +0700

    Revert "Revert "Tugas 2""
    This reverts commit f95e65d6001f354884103b17b43c61165a5c7291.

---
+++ Tugas 2/sembilan.java
@@ -0,0 +1,18 @@
+public class sembilan{
+	public static void main(String[] args) {
+		int i;
+		for (i=1;i<21 ;i++ ) {
+			if (i%5==0) {
+				System.out.print("JONI");
+			}
+			else {
+				System.out.print(i+" ");
+			}
+			if (i%11==0) {
+				System.out.print("\n");
+				
+			}
+			
+		}
+	}
+}
---
+++ Tugas 2/sepuluh.java
@@ -0,0 +1,13 @@
+public class sepuluh{
+	public static void main(String[] args) {
+		int n=11;
+		for (int i=1;i<=3 ;i++ ) {
+			System.out.println(" ");
+			for (int j=1;j<=3 ;j++ ) {
+				System.out.print("#");
+			
+			}
+
+		}
+	}
+}
