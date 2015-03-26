453b1a27608e5c90915b6d93cfcc7d4c43026d13
ardyputra <ardyputra96@gmail.com>
2015-03-26 16:17:24 +0700

    Tugas 2
    program Tugas 2

---
+++ Tugas 2/sebelas.java
@@ -0,0 +1,19 @@
+public class sebelas {
+	public static void main(String[] args) {
+		int i=7, j=9;
+		for (int h=1;h<=i ;h++ ) {
+			System.out.println(" ");
+			if (h%3==0) {
+				System.out.print(" #");
+			}
+			for (int l=1;l<=j;l++) {
+				System.out.print("#");
+
+			}
+				
+				
+		}
+	}
+}
+				
+			
